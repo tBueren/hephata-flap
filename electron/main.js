4f6e2d9e4cfe7342d4b72ce32a02d90598c83b02
Tobias Büren <tobias.bueren@gmail.com>
2026-09-21 11:32:18 +0200

    add electron

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    backgroundColor: '#1a1a1a',
+  });
+
+  win.loadFile(path.join(__dirname, '../dist/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
